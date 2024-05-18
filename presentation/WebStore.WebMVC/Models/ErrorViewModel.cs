24a3b9543d65cdb101d2736208676c5cfa1208eb
Kirill Poddubchak <kkirill_po@mail.ru>
2024-05-18 04:41:45 +0300

    Изменить веб-приложение на идентичное MVC приложение

---
+++ presentation/WebStore.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebStore.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
