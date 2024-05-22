ea7af2d855d482960415baae6fd0bdccee7f41b8
Kirill Poddubchak <kkirill_po@mail.ru>
2024-05-22 23:09:47 +0300

    Обновить сущность Book полями isbn и author

--- domain/WebStore/Book.cs
+++ domain/WebStore/Book.cs
@@ -4,7 +4,12 @@
     {
         public int Id { get; }
         public string Title { get; }
+        public string Isbn { get; }
+        public string Author { get; } //ненормализрованные данные - лучше использовать IdAuthor для обращения к сущности через идентификатор
 
-        public Book(int id, string title) { (Id, Title) = (id, title); }
+        public Book(int id, string isbn, string author,string title) 
+        { 
+            (Id, Isbn, Author, Title) = (id, isbn, author, title); 
+        }
     }
 }
--- infrastructure/WebStore.Memory/BookRepository.cs
+++ infrastructure/WebStore.Memory/BookRepository.cs
@@ -4,9 +4,9 @@
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "exemple book"),
-            new Book(2, "title book"),
-            new Book(3, "the last one")
+            new Book(1, "ISBN 12345-12345", "Some author", "exemple book"),
+            new Book(2, "ISBN 23456-12345", "Another author", "title book"),
+            new Book(3, "ISBN 34567-12345", "Last author", "the last one")
         };
         public Book[] GetAllBooksByTitle(string titlePart) 
         { 
